fc59fe3ca0dd33c9dcfd7c9cf94210dec80c6bf9
Robinson Bordes <robinson.bord@wanadoo.fr>
2017-12-12 12:18:49 +0100

    Premier commit sur la branche1

--- monappli-dao/src/main/java/com/objis/demomaven/App.java
+++ monappli-dao/src/main/java/com/objis/demomaven/App.java
@@ -1,13 +1,15 @@
 package com.objis.demomaven;
 
 /**
- * Hello world!
+ * Quand je fais un deuxième commit ca fait ca
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
+		System.out.println("Deuxième commit test");
+		System.out.println("Je suis sur ma branche 1");
         System.out.println( "Hello World!" );
     }
 }
